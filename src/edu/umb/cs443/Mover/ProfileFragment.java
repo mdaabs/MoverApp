50b82314d0d95fed2f108df42dae1bf0b0b6804c
Mario <mdaabs@gmail.com>
2014-11-30 00:50:01 -0500

    action bar changes

--- src/edu/umb/cs443/Mover/ProfileFragment.java
+++ src/edu/umb/cs443/Mover/ProfileFragment.java
@@ -3,6 +3,9 @@ package edu.umb.cs443.Mover;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -13,5 +16,29 @@ public class ProfileFragment extends Fragment {
 		View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
 		return view;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    // TODO Auto-generated method stub
+	    super.onCreate(savedInstanceState);
+
+	    setHasOptionsMenu(true);
+	    setRetainInstance(true);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public void onPrepareOptionsMenu(Menu menu) {
+	    super.onPrepareOptionsMenu(menu);
+
+	    MenuItem profile  = menu.findItem(R.id.edit_profile);
+	    profile.setVisible(true);
+
 	}
 }
--- src/edu/umb/cs443/Mover/StartActivity.java
+++ src/edu/umb/cs443/Mover/StartActivity.java
@@ -1,5 +1,6 @@
 package edu.umb.cs443.Mover;
 
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -11,26 +12,28 @@ import android.widget.Button;
 
 public class StartActivity extends FragmentActivity {
 
-	@Override
+	@SuppressLint("NewApi") @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		ActionBar actionBar = getActionBar();
-		/*actionBar.hide();*/
+		/* actionBar.hide(); */
+		getActionBar().setDisplayShowTitleEnabled(false);
+		actionBar.setIcon(android.R.color.transparent);
 		setContentView(R.layout.activity_start);
-		/*if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new StartFragment())
-                    .commit();
-        }*/
-		StartButtonsFragment mFragmentStart = new StartButtonsFragment();    
+		/*
+		 * if (savedInstanceState == null) {
+		 * getSupportFragmentManager().beginTransaction() .add(R.id.container,
+		 * new StartFragment()) .commit(); }
+		 */
+		StartButtonsFragment mFragmentStart = new StartButtonsFragment();
 		LogoFragment mFragmentLogo = new LogoFragment();
-		
-        FragmentTransaction transaction = 
-                getSupportFragmentManager().beginTransaction();
 
-        transaction.add(R.id.bottom_fragment, mFragmentStart);
-        transaction.add(R.id.top_fragment, mFragmentLogo);
-        transaction.commit();
+		FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
+
+		transaction.add(R.id.bottom_fragment, mFragmentStart);
+		transaction.add(R.id.top_fragment, mFragmentLogo);
+		transaction.commit();
 	}
 
 	@Override
@@ -46,18 +49,25 @@ public class StartActivity extends FragmentActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
 		return super.onOptionsItemSelected(item);
 	}
 	
 	@Override
-	public void onBackPressed() {
-	    if (getFragmentManager().getBackStackEntryCount() == 0) {
-	        super.onBackPressed();
-	    } else {
-	        getFragmentManager().popBackStack();
-	    }
+	public boolean onPrepareOptionsMenu(Menu menu) {
+	    super.onPrepareOptionsMenu(menu);
+
+	    MenuItem profile  = menu.findItem(R.id.edit_profile);
+	    profile.setVisible(false);
+	    
+	    return true;
 	}
+
+/*	@Override
+	public void onBackPressed() {
+		if (getFragmentManager().getBackStackEntryCount() == 0) {
+			super.onBackPressed();
+		} else {
+			getFragmentManager().popBackStack();
+		}
+	}*/
 }
